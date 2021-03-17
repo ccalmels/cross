f92323128ce3728eb7f8b5ca303dff8b88b6dd6a
Clement Calmels <clement.calmels@free.fr>
2021-03-17 19:59:31 +0100

    rust: use clap

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,9 @@
+extern crate clap;
+
+use clap::App;
+
 fn main() {
     println!("{}: Hello world!", std::env::args().next().unwrap());
+
+    let _ = App::new("foo").version("0.1.0").get_matches();
 }
